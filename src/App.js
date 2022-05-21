5390b6eddaf141b301a6ad6bc12f5af4a722d107
ro-ze1106 <85288297+ro-ze1106@users.noreply.github.com>
2022-05-21 20:32:44 +0900

    axiosの使用方法

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import axios from "axios";
 import "./styles.css";
 
 export default function App() {
+  const onClickUser = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  const onClickUse1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/3")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickUser}>users</button>
+      <button onClick={onClickUse1}>id=1のuser</button>
     </div>
   );
 }
